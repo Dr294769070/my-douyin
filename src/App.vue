b3b5fee9ab4cbaf39f6b4909870c8d87647e0a19
dingjinsong <jinsongding@sohu-inc.com>
2021-05-19 18:43:01 +0800

    code:初始化项目

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <login></login>
+  <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './pages/HelloWorld.vue'
+import Login from './pages/Login.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld,
+    Login
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/pages/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="red">
+            login
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.red{
+    color: red;
+}
+</style>
